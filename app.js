1f3e7893994cbc9e5d1f6a6d87fc8a2325dd43ba
ArielBonfil <stoari33@gmail.com>
2021-04-29 02:13:22 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+new Vue({
+  el: "#app",
+  data: {
+    saludJugador: 100,
+    saludMonstruo: 100,
+    hayUnaPartidaEnJuego: false,
+    turnos: [], //es para registrar los eventos de la partida
+    esJugador: false,
+    rangoAtaque: [3, 10],
+    rangoAtaqueEspecial: [10, 20],
+    rangoAtaqueDelMonstruo: [5, 12],
+  },
+
+  methods: {
+    getSalud(salud) {
+      return `${salud}%`;
+    },
+    empezarPartida: function () {
+      this.hayUnaPartidaEnJuego = true;
+      this.saludJugador = 100;
+      this.saludMonstruo = 100;
+    },
+    atacar: function () {},
+
+    ataqueEspecial: function () {},
+
+    curar: function () {},
+
+    registrarEvento(evento) {},
+    terminarPartida: function () {
+      this.hayUnaPartidaEnJuego = false;
+    },
+
+    ataqueDelMonstruo: function () {},
+
+    calcularHeridas: function (rango) {
+      return 0;
+    },
+    verificarGanador: function () {
+      return false;
+    },
+    cssEvento(turno) {
+      //Este return de un objeto es prque vue asi lo requiere, pero ponerlo acá queda mucho mas entendible en el codigo HTML.
+      return {
+        "player-turno": turno.esJugador,
+        "monster-turno": !turno.esJugador,
+      };
+    },
+  },
+});
